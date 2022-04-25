34b4915983df71dbbfb0a512fc78fe7edf4933d8
Valeria Vodolazova <vvv91@internet.ru>
2022-04-25 06:58:44 +0300

    added task 15

---
+++ Seminar 2 task 15/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число дня недели: ");
+int namber = int.Parse(Console.ReadLine());
+
+if(namber == 1)
+
+{
+    Console.WriteLine($"нет");
+}
+
+    if(namber == 2)
+    
+    {
+        Console.WriteLine($"нет");
+    }  
+
+        if(namber == 3)
+
+        {
+            Console.WriteLine($"нет");   
+        }
+
+            if(namber == 4)
+
+            {
+                Console.WriteLine($"нет");
+            }
+
+                if(namber == 5)
+
+                {
+                    Console.WriteLine($"нет");
+                }
+
+                    if(namber == 6)
+
+                    {
+                        Console.WriteLine($"да");
+                    }
+
+                        if(namber == 7)
+
+                        {
+                            Console.WriteLine($"да");
+                        }
