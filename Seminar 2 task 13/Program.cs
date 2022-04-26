4401b43663e191b02303804031ed31c681ad4a37
Valeria Vodolazova <vvv91@internet.ru>
2022-04-26 06:03:25 +0300

    added task 13

---
+++ Seminar 2 task 13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//Console.WriteLine("Введите число: ");
+//int namber = int.Parse(Console.ReadLine());
+
+//if (namber >= 1000 & namber < 10000)
+//{
+//    namber = namber / 10;
+//    namber = namber % 10;
+//    Console.WriteLine($"третьей цифра в данном числе = {namber}");
+//} else
+//    if (namber > 100 & namber < 1000)
+//    {
+//        namber = namber % 10;
+//        Console.WriteLine($"третьей цифра в данном числе = {namber}");
+ //   }
+//else Console.WriteLine("третьей цифры нет");
+
+
+Console.WriteLine("Введите число: ");
+int namber = int.Parse(Console.ReadLine());
+
+while(namber >= 1000)
+{
+    namber = namber / 10;
+}
+
+Console.WriteLine("третье число:" + namber % 10);
+
+if(namber < 100)
+
+{
+    Console.WriteLine("третьего числа нет");
+}
